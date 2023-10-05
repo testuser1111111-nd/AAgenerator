293bb5451f3ad23aefd8831fd26f319a7a73c362
testuser1111111-nd <tbsa334zn6@outlook.jp>
2023-10-05 11:21:48 +0900

    a

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -31,15 +31,14 @@
             button1 = new Button();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
-            checkBox1 = new CheckBox();
-            checkBox2 = new CheckBox();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(455, 27);
+            button1.Location = new Point(569, 34);
+            button1.Margin = new Padding(4, 4, 4, 4);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(118, 36);
             button1.TabIndex = 1;
             button1.Text = "Convert";
             button1.UseVisualStyleBackColor = true;
@@ -49,52 +48,33 @@
             // 
             textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             textBox1.Font = new Font("ＭＳ ゴシック", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
-            textBox1.Location = new Point(0, 86);
+            textBox1.Location = new Point(0, 108);
+            textBox1.Margin = new Padding(4, 4, 4, 4);
             textBox1.MaxLength = 100000;
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(800, 364);
+            textBox1.Size = new Size(999, 454);
             textBox1.TabIndex = 2;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(12, 27);
+            textBox2.Location = new Point(15, 34);
+            textBox2.Margin = new Padding(4, 4, 4, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(437, 27);
+            textBox2.Size = new Size(545, 31);
             textBox2.TabIndex = 3;
             textBox2.Text = "test.png";
             // 
-            // checkBox1
-            // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(558, 32);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(98, 24);
-            checkBox1.TabIndex = 6;
-            checkBox1.Text = "ASCII only";
-            checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(662, 32);
-            checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(99, 24);
-            checkBox2.TabIndex = 7;
-            checkBox2.Text = "Fast Mode";
-            checkBox2.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(checkBox2);
-            Controls.Add(checkBox1);
+            ClientSize = new Size(1000, 562);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Controls.Add(button1);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -105,7 +85,5 @@
         private Button button1;
         private TextBox textBox1;
         private TextBox textBox2;
-        private CheckBox checkBox1;
-        private CheckBox checkBox2;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -7,49 +7,11 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
-        public static List<Character> characters;
         public static (char, UInt128)[] ASCIIsarr;
         public Form1()
         {
             InitializeComponent();
-            LoadChrs();
             LoadASCIIs();
-        }
-        public static void LoadChrs()
-        {
-            characters = new List<Character>();
-            var files = Directory.EnumerateFiles(".\\images");
-            foreach (var file in files)
-            {
-                Bitmap img = (Bitmap)Image.FromFile(file);
-                string[] splitted = file.Split('\\');
-                var c = (char)int.Parse(splitted[splitted.Length - 1].Split('.')[0]);
-                if (c <= 127 | (0xff61 <= c & c <= 0xff9f))
-                {
-                    bool[,] bools = new bool[16, 8];
-                    for (int i = 0; i < 16; i++)
-                    {
-                        for (int j = 0; j < 8; j++)
-                        {
-                            bools[i, j] = img.GetPixel(j, i).GetBrightness() > 0.5;
-                        }
-                    }
-                    characters.Add(new Character(c, true, bools));
-                }
-                else
-                {
-                    bool[,] bools = new bool[16, 16];
-                    for (int i = 0; i < 16; i++)
-                    {
-                        for (int j = 0; j < 16; j++)
-                        {
-                            bools[i, j] = img.GetPixel(j, i).GetBrightness() > 0.5;
-                        }
-                    }
-                    characters.Add(new Character(c, false, bools));
-                }
-                img.Dispose();
-            }
         }
         public static void LoadASCIIs()
         {
@@ -83,7 +45,6 @@ namespace WinFormsApp1
             Bitmap img = (Bitmap)Image.FromFile(textBox2.Text);
             textBox1.Text += sw.Elapsed.ToString();
             textBox1.Text += "\r\n";
-
             bool[,] bools = new bool[img.Height, img.Width];
             for (int i = 0; i < img.Height; i++)
             {
@@ -118,40 +79,31 @@ namespace WinFormsApp1
             textBox1.Text += "\r\n";
             StringBuilder sb = new StringBuilder();
             string[] results = new string[divheight];
-            bool chkd = checkBox1.Checked;
-            bool fast = checkBox2.Checked;
-            if (chkd)
+            UInt128[][] converted = new UInt128[divheight][];
+            for(int i = 0; i < divheight; i++)
             {
-                UInt128[][] converted = new UInt128[divheight][];
-                for(int i = 0; i < divheight; i++)
+                converted[i] = new UInt128[divwidth];
+                for(int  j = 0; j < divwidth; j++)
                 {
-                    converted[i] = new UInt128[divwidth];
-                    for(int  j = 0; j < divwidth; j++)
+                    UInt128 ui = 0;
+                    for(int k = 0; k < 16; k++)
                     {
-                        UInt128 ui = 0;
-                        for(int k = 0; k < 16; k++)
+                        for(int l = 0; l < 8; l++)
                         {
-                            for(int l = 0; l < 8; l++)
-                            {
-                                ui <<= 1;
-                                ui |= (boolss[i][j][k, l]?1u:0);
-                            }
+                            ui <<= 1;
+                            ui |= (boolss[i][j][k, l]?1u:0);
                         }
-                        converted[i][j] = ui;
                     }
+                    converted[i][j] = ui;
                 }
-                Parallel.For(0, divheight, i => { results[i] = ASCIItask(divwidth, converted[i]).Result; });
-                for (int i = 0; i < divheight; i++) sb.Append(results[i]);
-                //textBox1.Text += sb.ToString();
-            }
-            else
-            {
-                Parallel.For(0, divheight, i => { results[i] =task(divwidth, boolss, i,fast).Result; });
-                for (int i = 0; i < divheight; i++) sb.Append(results[i]);
-                //textBox1.Text = sb.ToString();
             }
             textBox1.Text += sw.Elapsed.ToString();
-            textBox1.Text += "\n";
+            textBox1.Text += "\r\n";
+            Parallel.For(0, divheight, i => { results[i] = ASCIItask(divwidth, converted[i]).Result; });
+            for (int i = 0; i < divheight; i++) sb.Append(results[i]);
+            //textBox1.Text += sb.ToString();
+            textBox1.Text += sw.Elapsed.ToString();
+            textBox1.Text += "\r\n";
             GC.Collect();
         }
         public Task<string> ASCIItask(in long divwidth, in UInt128[] image)
@@ -176,222 +128,10 @@ namespace WinFormsApp1
             sb.AppendLine();
             return Task.FromResult<string>(sb.ToString());
         }
-        public Task<string> task(in long divwidth, in bool[][][,] boolss, in long i,in bool fast)
-        {
-            StringBuilder sb = new StringBuilder();
-            //コスト
-            double[] dp = new double[divwidth + 1];
-            dp[0] = 0;
-            for (int j = 1; j <= divwidth; j++)
-            {
-                dp[j] = double.MaxValue;
-            }
-            int[] prev = new int[divwidth + 1];
-            char[] prevchr = new char[divwidth + 1];
-            for (int j = 0; j < divwidth; j++)
-            {
-                bool[,] hanspace = new bool[16, 8];
-                for (int k = 0; k < 16; k++)
-                {
-                    for (int l = 0; l < 8; l++)
-                    {
-                        hanspace[k, l] = true;
-                    }
-                }
-                double nextcost = dp[j] + EvalHan(fast, hanspace, boolss[i][j]);
-                char nextchr = ' ';
-                foreach (var character in characters)
-                {
-                    if (!character.ishan) continue;
-                    if (nextcost > dp[j] + EvalHan(fast, character.Bools, boolss[i][j]))
-                    {
-                        nextchr = character.c;
-                        nextcost = dp[j] + EvalHan(fast, character.Bools, boolss[i][j]);
-                    }
-                }
-                if (nextcost < dp[j + 1])
-                {
-                    dp[j + 1] = nextcost;
-                    prev[j + 1] = j;
-                    prevchr[j + 1] = nextchr;
-                }
-                if (j + 2 <= divwidth)
-                {
-                    bool zenexists = false;
-                    bool[,] zenspace = new bool[16, 16];
-                    bool[,] combined = new bool[16, 16];
-                    for (int k = 0; k < 16; k++)
-                    {
-                        for (int l = 0; l < 8; l++)
-                        {
-                            combined[k, l] = boolss[i][j][k, l];
-                            combined[k, l + 8] = boolss[i][j + 1][k, l];
-                        }
-                        for (int l = 0; l < 16; l++)
-                        {
-                            zenspace[k, l] = true;
-                        }
-                    }
-                    double zennextcost = dp[j] + EvalZen(fast, zenspace, combined);
-                    char zennextchr = '　';
-                    foreach (var zencharacter in characters)
-                    {
-                        if (zencharacter.ishan) continue;
-                        zenexists = true;
-                        if (zennextcost > dp[j] + EvalZen(fast, zencharacter.Bools, combined))
-                        {
-                            zennextchr = zencharacter.c;
-                            zennextcost = dp[j] + EvalZen(fast, zencharacter.Bools, combined);
-                        }
-                    }
-                    if (zennextcost < dp[j + 2] & zenexists)
-                    {
-                        dp[j + 2] = zennextcost;
-                        prev[j + 2] = j;
-                        prevchr[j + 2] = zennextchr;
-                    }
-                }
-
-            }
-            Stack<char> stack = new Stack<char>();
-            long now = divwidth;
-            while (now != 0)
-            {
-                stack.Push(prevchr[now]);
-                now = prev[now];
-            }
-            while (stack.Count > 0)
-            {
-                sb.Append(stack.Pop());
-            }
-            sb.AppendLine();
-            return Task.FromResult<string>(sb.ToString());
-        }
-
-
-
-        private double EvalZen(bool fastmode, bool[,] x, bool[,] y)
-        {
-            if (fastmode)
-            {
-                long cost = 0;
-                for (int i = 0; i < 16; i++)
-                {
-                    for (int j = 0; j < 16; j++)
-                    {
-                        cost += (x[i, j] != y[i, j]) ? 1 : 0;
-                    }
-                }
-                return (double)cost;
-            }
-            else
-            {
-                double cost = 0;
-                for (int i = 0; i < 16; i++)
-                {
-                    for (int j = 0; j < 16; j++)
-                    {
-                        double tempcost = 3;
-                        double tempcost2 = 3;
-                        for (int k = -3; k <= 3; k++)
-                        {
-                            for (int l = -3; l <= 3; l++)
-                            {
-                                if (0 <= i + k & i + k < 16 & 0 <= j + l & j + l < 16)
-                                {
-                                    if (x[i, j] == y[i + k, j + l])
-                                    {
-                                        tempcost = Math.Min(tempcost, Math.Sqrt(k * k + l * l));
-                                    }
-
-                                    if (y[i, j] == x[i + k, j + l])
-                                    {
-                                        tempcost2 = Math.Min(tempcost2, Math.Sqrt(k * k + l * l));
-                                    }
-
-                                }
-                            }
-                        }
-                        cost += tempcost + tempcost2;
-                    }
-                }
-                return cost;
-            }
-        }
-
-        private double EvalHan(bool fastmode, bool[,] x, bool[,] y)
-        {
-            if( fastmode )
-            {
-                long cost = 0;
-                for (int i = 0; i < 16; i++)
-                {
-                    for (int j = 0; j < 8; j++)
-                    {
-                        cost += (x[i, j] != y[i, j]) ? 1 : 0;
-                    }
-                }
-                return cost;
-            }
-            else
-            {
-                double cost = 0;
-                for (int i = 0; i < 16; i++)
-                {
-                    for (int j = 0; j < 8; j++)
-                    {
-                        double tempcost = 3;
-                        double tempcost2 = 3;
-                        for (int k = -3; k <= 3; k++)
-                        {
-                            for (int l = -3; l <= 3; l++)
-                            {
-                                if (0 <= i + k & i + k < 16 & 0 <= j + l & j + l < 8)
-                                {
-                                    if (x[i, j] == y[i + k, j + l])
-                                    {
-                                        tempcost = Math.Min(tempcost, Math.Sqrt(k * k + l * l));
-                                    }
-
-                                    if (y[i, j] == x[i + k, j + l])
-                                    {
-                                        tempcost2 = Math.Min(tempcost2, Math.Sqrt(k * k + l * l));
-                                    }
-
-                                }
-                            }
-                        }
-                        cost += tempcost + tempcost2;
-                    }
-                }
-                return cost;
-            }
-        }
-
-
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
-
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
     }
-    public class Character
-    {
-        public char c;
-        public bool ishan;
-        public bool[,] Bools;
-        public Character(char c, bool ishan, bool[,] bools) 
-        {
-            this.c = c;
-            this.ishan = ishan;
-            this.Bools = bools;
-        }
-    }
-
-
 }
